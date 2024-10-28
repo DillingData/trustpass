95d9114fd8157f281993d44bc23c6c55bd3d8520
Thomas <thomas@dillingdata.se>
2024-10-28 06:37:09 +0100

    Working on the passwordhandler, creating a function that checks if a password is safe

--- src/helperFunctions/passWordHandling.tsx
+++ src/helperFunctions/passWordHandling.tsx
@@ -1,5 +1,19 @@
 
 export function checkPassword (password: string) {
-    
-    return true;
+
+    var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+    if (password.length < 12) {
+        return false;
+    } else {
+        if (format.test(password)) {
+            if (/\d/.test(password)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
